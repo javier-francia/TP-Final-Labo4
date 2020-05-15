907f49b9a5166df3f77a116045e2cf7717837a84
Javier Francia Peñarrocha <javifrancia@gmail.com>
2020-05-15 14:05:42 -0300

    Agrego referencias al firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
+//            Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+//            Components
 import { AppComponent } from './app.component';
+
+//            Environment
+import { environment } from '../environments/environment.prod';
+
 
 @NgModule({
   declarations: [
@@ -10,6 +19,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
