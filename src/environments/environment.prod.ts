696a8fbda04dee49016514fdadfab9a853ff16a6
Javier Francia Peñarrocha <javifrancia@gmail.com>
2020-05-15 10:54:43 -0300

    Commit inicial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCtcfjvpLVUXqZqoxeO4W97aa4WohYUloo",
+    authDomain: "final-tp-javier-francia.firebaseapp.com",
+    databaseURL: "https://final-tp-javier-francia.firebaseio.com",
+    projectId: "final-tp-javier-francia",
+    storageBucket: "final-tp-javier-francia.appspot.com",
+    messagingSenderId: "252111642848",
+    appId: "1:252111642848:web:1cec70ed1f198f2901b085"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCtcfjvpLVUXqZqoxeO4W97aa4WohYUloo",
+    authDomain: "final-tp-javier-francia.firebaseapp.com",
+    databaseURL: "https://final-tp-javier-francia.firebaseio.com",
+    projectId: "final-tp-javier-francia",
+    storageBucket: "final-tp-javier-francia.appspot.com",
+    messagingSenderId: "252111642848",
+    appId: "1:252111642848:web:1cec70ed1f198f2901b085"
+  }
 };
 
 /*
